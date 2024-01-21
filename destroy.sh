88e08cf2469d23b269b2934de0f0161eccb4ee92
cybersharkvin <103551471+cybersharkvin@users.noreply.github.com>
2024-01-20 21:51:51 -0600

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # Define the role and user names
-ROLE_NAME="ADMIN1"
+ROLE_NAME1="ADMIN1"
+ROLE_NAME2="ADMIN2"
 USER1="LabUser1"
 USER2="LabUser2"
 
-# Detach policy from the role
-aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+# Function to delete a role
+delete_role() {
+    local role_name=$1
 
-# Delete the role
-aws iam delete-role --role-name $ROLE_NAME
+    # Detach all managed policies from the role
+    for policy_arn in $(aws iam list-attached-role-policies --role-name $role_name --query 'AttachedPolicies[*].PolicyArn' --output text); do
+        aws iam detach-role-policy --role-name $role_name --policy-arn $policy_arn
+    done
+
+    # Finally, delete the role
+    aws iam delete-role --role-name $role_name
+}
 
 # Function to delete a user
 delete_user() {
@@ -20,7 +28,7 @@ delete_user() {
         aws iam delete-access-key --user-name $username --access-key-id $key
     done
 
-    # Detach user policies
+    # Detach all managed policies from the user
     for policy_arn in $(aws iam list-attached-user-policies --user-name $username --query 'AttachedPolicies[*].PolicyArn' --output text); do
         aws iam detach-user-policy --user-name $username --policy-arn $policy_arn
     done
@@ -34,6 +42,10 @@ delete_user() {
     aws iam delete-user --user-name $username
 }
 
+# Delete roles
+delete_role $ROLE_NAME1
+delete_role $ROLE_NAME2
+
 # Delete users
 delete_user $USER1
 delete_user $USER2
@@ -41,5 +53,5 @@ delete_user $USER2
 # Remove the script directory
 rm -rf AWS-Admin-Role-Creation-Script/
 
-echo "Role $ROLE_NAME and users $USER1, $USER2 along with their policies and access keys have been deleted."
+echo "Roles $ROLE_NAME1 and $ROLE_NAME2, and users $USER1, $USER2 along with their policies and access keys have been deleted."
 echo "AWS-Admin-Role-Creation-Script directory has been removed."
